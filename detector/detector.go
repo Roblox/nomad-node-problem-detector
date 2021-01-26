583e4f9dd7725a244b66c4c1d164ab7ebff660e6
Shishir Mahajan <smahajan@roblox.com>
2021-01-26 14:43:16 -0800

    Add http handlers.

--- detector/detector.go
+++ detector/detector.go
@@ -2,9 +2,28 @@ package detector
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func StartNpdHttpServer() {
-	fmt.Println("Hello nomad node problem detector!")
+func StartNpdHttpServer() error {
+	fmt.Println("Starting nomad node problem detector...")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Nomad node problem detector!\n"))
+	})
+	http.HandleFunc("/v1/health/", healthCheckHandler)
+	http.HandleFunc("/v1/nodehealth/", nodeHealthHandler)
+	if err := http.ListenAndServe(":8083", nil); err != nil {
+		return err
+	}
+	return nil
+}
 
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Calling /v1/health/")
+	w.WriteHeader(http.StatusOK)
+}
+
+func nodeHealthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Calling /v1/nodehealth/")
+	w.WriteHeader(http.StatusOK)
 }
