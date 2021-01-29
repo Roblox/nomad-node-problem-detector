810e26a14619f8755f208c69ccc7a87d9cccbe3f
Shishir Mahajan <smahajan@roblox.com>
2021-01-28 18:11:19 -0800

    Add support for npd --aggregator.

--- aggregator/aggregator.go
+++ aggregator/aggregator.go
@@ -1,9 +1,79 @@
 package aggregator
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/hashicorp/nomad/api"
 )
 
 func Aggregate() {
-	fmt.Println("Hello aggregator mode!")
+	client, err := getNomadClient()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	queryOptions := &api.QueryOptions{AllowStale: true}
+
+	for {
+		fmt.Println("Collect and aggregate nodes health")
+		nodes, _, err := client.Nodes().List(queryOptions)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for _, node := range nodes {
+			url := fmt.Sprintf("http://%s:8083/v1/nodehealth", node.Address)
+			req, err := http.NewRequest("POST", url, nil)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			req.Header.Set("Content-Type", "application/json")
+			client := &http.Client{Timeout: time.Second * 5}
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			var out bytes.Buffer
+			err = json.Indent(&out, body, "", "    ")
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			out.WriteTo(os.Stdout)
+			fmt.Println()
+		}
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func getNomadClient() (*api.Client, error) {
+	cfg := api.DefaultConfig()
+	cfg.Address = "http://localhost:4646"
+	cfg.TLSConfig.Insecure = true
+
+	timeout, err := time.ParseDuration("5s")
+	if err != nil {
+		return nil, err
+	}
+
+	cfg.HttpClient = &http.Client{Timeout: timeout}
+	client, err := api.NewClient(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/nomad-node-problem-detector
 
 go 1.15
 
-require github.com/urfave/cli/v2 v2.3.0
+require (
+	github.com/hashicorp/nomad/api v0.0.0-20210128220232-4b7ee2269213
+	github.com/urfave/cli/v2 v2.3.0
+)
