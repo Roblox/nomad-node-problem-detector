e09e74235bfb63f9cec598330b29dfb6b7fdd394
Shishir Mahajan <smahajan@roblox.com>
2021-02-01 14:37:28 -0800

    Check nNPD /v1/health/ before gathering node health.

--- aggregator/aggregator.go
+++ aggregator/aggregator.go
@@ -23,16 +23,34 @@ func Aggregate() {
 	queryOptions := &api.QueryOptions{AllowStale: true}
 
 	for {
-		fmt.Println("Collect and aggregate nodes health")
+		log.Info("Collect and aggregate nodes health")
 		nodes, _, err := nodeHandle.List(queryOptions)
 		if err != nil {
 			errMsg := fmt.Sprintf("Error in listing nomad nodes: %v\n", err)
 			log.Warning(errMsg)
+			time.Sleep(15 * time.Second)
 			continue
 		}
 
 		for _, node := range nodes {
-			url := fmt.Sprintf("http://%s:8083/v1/nodehealth/", node.Address)
+			npdServer := fmt.Sprintf("http://%s:8083", node.Address)
+
+			npdActive, err := isNpdServerActive(npdServer)
+			if err != nil {
+				errMsg := fmt.Sprintf("Node %s is unreachable, marking it as ineligible.", node.Address)
+				log.Warning(errMsg)
+				toggleNodeEligibility(nodeHandle, node.ID, node.Address, false)
+				continue
+			}
+
+			if !npdActive {
+				errMsg := fmt.Sprintf("Node %s is unhealthy, marking it as ineligible.", node.Address)
+				log.Warning(errMsg)
+				toggleNodeEligibility(nodeHandle, node.ID, node.Address, false)
+				continue
+			}
+
+			url := npdServer + "/v1/nodehealth/"
 			req, err := http.NewRequest("POST", url, nil)
 			if err != nil {
 				errMsg := fmt.Sprintf("Error in building /v1/nodehealth/ HTTP request, skipping node %s\n", node.Address)
@@ -68,35 +86,65 @@ func Aggregate() {
 			}
 
 			nodeHealthy := true
-			toggleEligibilityError := false
+			toggleOK := true
 			for _, res := range result {
 				if res.Result != "Healthy" {
-					log.Info(fmt.Sprintf("Node %s is not healthy, marking it as ineligible.", node.Address))
-					if _, err := nodeHandle.ToggleEligibility(node.ID, false, nil); err != nil {
-						errMsg := fmt.Sprintf("Error in toggling node eligibility, skipping node %s\n", node.Address)
-						log.Warning(errMsg)
-						toggleEligibilityError = true
-						break
-					}
+					errMsg := fmt.Sprintf("Node %s is not healthy, marking it as ineligible.", node.Address)
+					log.Warning(errMsg)
+					toggleOK = toggleNodeEligibility(nodeHandle, node.ID, node.Address, false)
 					nodeHealthy = false
 					break
 				}
 			}
 
-			if toggleEligibilityError {
+			if !toggleOK {
+				// Skip node
 				continue
 			}
 
 			if nodeHealthy {
-				if _, err := nodeHandle.ToggleEligibility(node.ID, true, nil); err != nil {
-					log.Fatalln(err)
-				}
+				toggleNodeEligibility(nodeHandle, node.ID, node.Address, true)
 			}
 		}
-		time.Sleep(3 * time.Second)
+		time.Sleep(15 * time.Second)
 	}
 }
 
+// Toggle Nomad node eligibility, and returns a boolean to indicate if the operation was successful
+// or unsuccessful.
+func toggleNodeEligibility(nodeHandle *api.Nodes, nodeID, nodeAddress string, eligible bool) bool {
+	if _, err := nodeHandle.ToggleEligibility(nodeID, eligible, nil); err != nil {
+		errMsg := fmt.Sprintf("Error in toggling node eligibility, skipping node %s\n", nodeAddress)
+		log.Warning(errMsg)
+		return false
+	}
+	return true
+}
+
+// Check if Nomad node problem detector (nNPD) HTTP server is healthy and active.
+func isNpdServerActive(npdServer string) (bool, error) {
+	url := npdServer + "/v1/health/"
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return false, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{Timeout: time.Second * 5}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return false, nil
+	}
+	return true, nil
+}
+
+// Get Nomad HTTP client.
+// This client will be used to list nodes and toggle node eligibility.
 func getNomadClient() (*api.Client, error) {
 	cfg := api.DefaultConfig()
 	cfg.Address = "http://localhost:4646"
