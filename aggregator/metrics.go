0e2eeb1e5786a7cc9906d1d4dc6d765e22dcbfe0
fcuny-rbx <98835311+fcuny-rbx@users.noreply.github.com>
2022-03-25 15:47:55 -0700

    aggregator: export various metrics (#39)
    * aggregator: export various metrics
    
    Add two new arguments to the aggregator, to configure the address and
    port the HTTP endpoint will use to expose metrics. By default, the
    server binds on all interfaces and on port 3000.
    
    There's a number of metrics being exposed. The first two are runtime
    metrics (process and go's runtime).
    
    The application specific metrics are:
    
    - aggregator_cycles_total: the total number of cycles the aggregator has
      gone  through - this metric can be used to ensure the aggregator is
      still  making progress
    
    - aggregator_processing_time: how long the aggregator spend during the
      most recent cycle. This metric can be used to detect possible
      degradation in performance of the aggregator
    
    - nodes_total: how many nodes are returned by nomad this cycle
    
    - nodes_eligible: how many nodes are eligible this cycle
    
    - node_handle_errors: how many errors while processing the nodes - this
      metric can be used to detect spikes in failures, or to ensure the
      aggregator is not failing on a large number of hosts
    
    - node_handle_skips: how many nodes have been skipped - this metric can
      be used to detect spiked in hosts where the detector is not working
    
    - node_healthy: how many nodes are marked as healthy - this metric
      can be used to ensure that the majority of hosts are healthy. This can
      be used as a baseline for an SLO (e.g. 99% of the hosts in the cluster
      are reported as healthy)
    
    - node_unhealthy: how many nodes are marked as unhealthy. This metric
      can be used to detect spikes in hosts becoming unhealthy, or slow
      degradation over time (e.g. 2% of the hosts have become unhealthy in
      the last 24 hours)
    
    - node_health_state_changes: how many nodes have their health state
      changed. A sudden change in this metric can help surfacing possible
      network or infrastructure errors
    
    * aggregator: focus the metrics on the health checks
    
    Instead of collecting metrics around how many hosts are healthy or
    unhealthy, it's more useful to focus the metrics around the health
    checks themselves.
    
    These metrics are more useful:
    ```
    node_healthy{check="containerd",dc="foo"} 1
    node_healthy{check="portworx",dc="foo"} 1
    node_unhealthy{check="docker",dc="foo",host="10.0.2.15"} 1
    ```
    
    `node_healthy` collects how many hosts are healthy for each health
    check. This helps after rolling out a new health check to ensure it's
    being used and that hosts are indeed healthy.
    
    `node_unhealthy` collect which health checks failed and for which host,
    which can be useful for alerting or diagnosing.
    
    `node_healthy` does not collect per host success, to avoid an explosion
    of cardinality.
    
    * remove metric for `healthCheckStateChangedCounter`
    
    * update README and add message to healthCheckUnhealthyCounter

---
+++ aggregator/metrics.go
@@ -0,0 +1,89 @@
+package aggregator
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	aggregatorCyclesTotalCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "aggregator_cycles_total",
+			Help: "Number of cycles",
+		}, []string{"dc"})
+
+	aggregatorProcessingTime = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "aggregator_processing_time",
+			Help: "time spend aggregating information for this cycle",
+		}, []string{"dc"})
+
+	nodesTotalGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "nodes_total",
+			Help: "Total number of nodes",
+		}, []string{"dc"})
+
+	eligibleNodesGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "nodes_eligible",
+			Help: "Number of eligible nodes for this cycle",
+		}, []string{"dc"})
+
+	nodeHandleErrorsCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "node_handle_errors",
+			Help: "Count of errors while handling a node",
+		}, []string{"dc"})
+
+	nodeHandleSkipCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "node_handle_skip",
+			Help: "Count of nodes skipped",
+		}, []string{"dc"})
+
+	healthCheckHealthyCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "node_healthy",
+			Help: "Count of healthy nodes",
+		}, []string{"dc", "check"})
+
+	healthCheckUnhealthyCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "node_unhealthy",
+			Help: "Count of unhealthy nodes",
+		}, []string{"dc", "check", "host", "message"})
+)
+
+func registerMetrics() *prometheus.Registry {
+	r := prometheus.NewRegistry()
+	r.MustRegister(aggregatorCyclesTotalCounter)
+	r.MustRegister(aggregatorProcessingTime)
+	r.MustRegister(eligibleNodesGauge)
+	r.MustRegister(nodesTotalGauge)
+	r.MustRegister(nodeHandleErrorsCounter)
+	r.MustRegister(nodeHandleSkipCounter)
+	r.MustRegister(healthCheckHealthyCounter)
+	r.MustRegister(healthCheckUnhealthyCounter)
+	r.MustRegister(prometheus.NewGoCollector())
+	r.MustRegister(prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}))
+	return r
+}
+
+func metricsExporter(exporterAddr string, exporterPort int) {
+	addr := net.JoinHostPort(exporterAddr, strconv.Itoa(exporterPort))
+
+	registry := registerMetrics()
+	go func() {
+		mux := http.NewServeMux()
+		mux.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
+		if err := http.ListenAndServe(addr, mux); err != nil {
+			log.Fatalf("Failed to start Prometheus scrape endpoint: %v", err)
+		}
+	}()
+}
